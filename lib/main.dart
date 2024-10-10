c906d28fc24ce9bea9b74d107e7acd832ae34e29
mariovitor <mvitor.f@gmail.com>
2024-10-10 09:50:29 -0300

    set preferred orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:desafio_auth_totp/src/core/locator.dart';
 import 'package:desafio_auth_totp/src/login_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Locator().initDependencies();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const LoginApp());
 }
